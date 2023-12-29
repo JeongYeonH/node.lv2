956f9075fc44fe99f7934be8ad624ff504783186
jennaaaaaaaaa <ggm4954@gmail.com>
2023-12-29 23:06:56 +0900

    리뷰기능

--- src/routes/comments.router.js
+++ src/routes/comments.router.js
@@ -4,6 +4,7 @@ import {prisma} from '../utils/prisma/index.js'
 
 const router = express.Router()
 
+//목록조회
 router.get('/', async(req, res, next) => {
     return res.status(200).json({messgae:'hi'})
 })
