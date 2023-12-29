ee581f82db51d14765dcde26c9f4ddf7a884bbd8
jennaaaaaaaaa <ggm4954@gmail.com>
2023-12-29 16:40:35 +0900

    파일기본구조

--- src/app.js
+++ src/app.js
@@ -1,4 +1,6 @@
 import express from 'express'
+import reviewsRouter from './routes/reviews.router'
+import commentsRouter from './routes/comments.router'
 
 const app = express()
 const port = 3000
@@ -6,7 +8,7 @@ const port = 3000
 app.use(express.json())
 app.use(express.urlencoded({extended:true}))
 
-app.use('/api', [])
+app.use('/api', [commentsRouter, reviewsRouter])
 
 app.listen(port, () => {
     console.log(port, "서버에 연결되었습니다.")
