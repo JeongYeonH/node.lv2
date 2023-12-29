a152379a92d44ed04a46a5169450f6bd9395d01d
jennaaaaaaaaa <ggm4954@gmail.com>
2023-12-29 16:46:24 +0900

    수정

--- src/app.js
+++ src/app.js
@@ -1,12 +1,12 @@
 import express from 'express';
-import ReviewRouter from './routes/reviews.router.js'
-import CommentRouter from './routes/comments.router.js'
+import reviewsRouter from './routes/reviews.router.js'
+import commentsRouter from './routes/comments.router.js'
 
 const app = express();
 const PORT = 3017;
 
 app.use(express.json());
-app.use('/api', [ReviewRouter, CommentRouter]);
+app.use('/api', [reviewsRouter, commentsRouter]);
 
 app.listen(PORT, () => {
   console.log(PORT, '포트로 서버가 열렸어요!');
