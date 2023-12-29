f3ea3b69b52d0bbae8ae32c3c697ee54400cd0ad
Jeong Yeon Hwang <kukuro1995@naver.com>
2023-12-29 16:33:33 +0900

    내 브랜치

--- src/app.js
+++ src/app.js
@@ -1,12 +1,21 @@
 import express from 'express'
 
-const app = express()
-const port = 3000
 
-app.use(express.json())
-app.use(express.urlencoded({extended:true}))
+const app = express();
+const port = 3000;
 
-app.use('/api', [])
+
+
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+
+const router = express.Router();
+
+router.get('/', (req, res) =>{
+    return res.json({message: 'Hi!'});
+});
+
+app.use('/api', [router]);
 
 app.listen(port, () => {
     console.log(port, "서버에 연결되었습니다.")
